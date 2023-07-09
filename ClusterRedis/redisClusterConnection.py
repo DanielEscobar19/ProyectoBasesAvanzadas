c07b35c710bf8a3453e8871c3770cae4bfd04263
DanielEscobar19 <danielonchio@gmail.com>
2023-07-09 00:51:49 -0600

    Falta ejemplo de python

--- ClusterRedis/redisClusterConnection.py
+++ ClusterRedis/redisClusterConnection.py
@@ -4,15 +4,8 @@ from rediscluster import RedisCluster
 startup_nodes = [{"host": "127.0.0.1", "port": "7001"}, {"host": "127.0.0.1", "port": "7002"}]
 redisCluster = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
 
-redisCluster.set("foo", "bar")
+redisCluster.hset(f'user_1', mapping={'name': 'Gerardo', 'edad': 20})
 
-foo = redisCluster.get("foo")
+usuarioLeido = redisCluster.hgetall("user_1")
 
-print(f'result {foo}')
-
-redisCluster.set("usuario", "Viernes")
-
-usuario = redisCluster.get("usuario")
-
-print(f'result {usuario}')
-
+print(f'result {usuarioLeido}')
